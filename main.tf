337beb9e58716652556416fedd8fcf28a5011018
brokebastard <brokebastard@users.noreply.github.com>
2020-04-09 14:40:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ resource "aws_instance" "scalr" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = var.subnet
-  vpc_security_group_ids = var.sg
   key_name               = var.key
   tags = {
     Name = var.instance_name
